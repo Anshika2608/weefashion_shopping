20d47311e7ca85a1331d7e41cf3809929a76c023
Anshika2608 <144312062+Anshika2608@users.noreply.github.com>
2024-08-02 15:05:30 +0530

    Update KidsContext.jsx

--- Client/src/Contexts/KidsContext/KidsContext.jsx
+++ Client/src/Contexts/KidsContext/KidsContext.jsx
@@ -12,7 +12,7 @@ export const KidsContextProvider = ({ children }) => {
   const [boterror, setboterror] = useState(null);
   const [footerror, setfooterror] = useState(null);
   const [loading, setLoading] = useState(true);
-  const url="http://localhost:5000"
+  const url="https://weefashion-shopping-backend-01lg.onrender.com"
   useEffect(() => {
     setLoading(true);
     const timer = setTimeout(() => {
