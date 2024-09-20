28221aeaefa98fe6d3e231366b5454cd41884bd2
Anshika2608 <anshika2210142@akgec.ac.in>
2024-09-20 23:57:25 +0530

    handleSubmit_function_defined

--- Client/src/Pages/Place_Order/PlaceOrder.jsx
+++ Client/src/Pages/Place_Order/PlaceOrder.jsx
@@ -2,6 +2,7 @@ import React,{useState} from 'react'
 import axios from "axios";
 
 function PlaceOrder() {
+    const url="https://weefashion-backend.onrender.com"
     const [formData, setFormData] = useState({
         fname: "",
         lname: "",
@@ -13,8 +14,21 @@ function PlaceOrder() {
         country: "",
         phone: ""
     });
-    const handleSubmit=async()=>{
+    const handleChange = (e) => {
+        const { name, value } = e.target;
 
+        const newValue = name === 'phone' || name === 'zipcode' ? Number(value) : value;
+
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: newValue,
+        }));
+    };
+    const handleSubmit=async()=>{
+       const response=await axios.post(`${url}/order/`,{
+              fname,lname,email,street,city,state,zipCode,country,phone
+       })
+       setFormData(response.data)
     }
     return (
         <>
@@ -23,28 +37,28 @@ function PlaceOrder() {
                 <div className='flex flex-col gap-4 w-full  sm:max-w-[480px] ml-20 mt-10'>
                     <h1 className='text-3xl font-semibold my-3 mx-20'>Delivery Information</h1>
                     <div className='flex  justify-between gap-2'>
-                        <input type="text" placeholder='first name' name="fname" value={formData.fname} className='p-4 w-full h-10 border-2 border-gray-300 rounded' />
-                        <input type="text" placeholder='last name' name="lname"   value={formData.lname} className='p-4 w-full h-10 border-2  border-gray-300 rounded' />
+                        <input type="text" placeholder='first name' name="fname" value={formData.fname} onChange={handleChange}  className='p-4 w-full h-10 border-2 border-gray-300 rounded' />
+                        <input type="text" placeholder='last name' name="lname"   value={formData.lname} onChange={handleChange}  className='p-4 w-full h-10 border-2  border-gray-300 rounded' />
 
                     </div>
                     <div>
-                        <input type="email" placeholder='Email' name="email"  value={formData.email} className='p-4 w-full h-10 border-2  border-gray-300 rounded' />
+                        <input type="email" placeholder='Email' name="email"  value={formData.email} onChange={handleChange} className='p-4 w-full h-10 border-2  border-gray-300 rounded' />
                     </div>
                     <div>
-                        <input type="text" placeholder='Street' name="street"   value={formData.street} className='p-4 w-full h-10   border-2 border-gray-300 rounded' />
+                        <input type="text" placeholder='Street' name="street"   value={formData.street} onChange={handleChange} className='p-4 w-full h-10   border-2 border-gray-300 rounded' />
                     </div>
                     <div className='flex  justify-between gap-2'>
-                        <input type="text" placeholder='City' name="city"  value={formData.city}  className='p-4 w-full h-10 border-2 border-gray-300 rounded' />
-                        <input type="text" placeholder='State' name="state" className='p-4 w-full h-10 border-2  border-gray-300 rounded' />
+                        <input type="text" placeholder='City' name="city"  value={formData.city} onChange={handleChange} className='p-4 w-full h-10 border-2 border-gray-300 rounded' />
+                        <input type="text" placeholder='State' name="state" onChange={handleChange} className='p-4 w-full h-10 border-2  border-gray-300 rounded' />
 
                     </div>
                     <div className='flex  justify-between gap-2'>
-                        <input type="number" name="zipCode" placeholder='Zipcode' value={formData.zipCode} className='p-4 w-full h-10 border-2 border-gray-300 rounded' />
-                        <input type="text" name="country" placeholder='Country' value={formData.country} className='p-4 w-full h-10 border-2  border-gray-300 rounded' />
+                        <input type="number" name="zipCode" placeholder='Zipcode' onChange={handleChange} value={formData.zipCode} className='p-4 w-full h-10 border-2 border-gray-300 rounded' />
+                        <input type="text" name="country" placeholder='Country' onChange={handleChange} value={formData.country} className='p-4 w-full h-10 border-2  border-gray-300 rounded' />
 
                     </div>
                     <div>
-                        <input type="Phone" placeholder=' Contact Number' value={formData.phone} name="Phone" className='p-3 w-full h-10   border-2 border-gray-300 rounded' />
+                        <input type="Phone" placeholder=' Contact Number' onChange={handleChange} value={formData.phone} name="Phone" className='p-3 w-full h-10   border-2 border-gray-300 rounded' />
 
                     </div>
                     <div className='self-center'>
