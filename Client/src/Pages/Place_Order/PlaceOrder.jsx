5188162c2e5c33ee34a8b751492b8f2c332fbb6e
Anshika2608 <anshika2210142@akgec.ac.in>
2024-09-20 23:49:28 +0530

    address_useState_defined

--- Client/src/Pages/Place_Order/PlaceOrder.jsx
+++ Client/src/Pages/Place_Order/PlaceOrder.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React,{useState} from 'react'
+import axios from "axios";
 
 function PlaceOrder() {
+    const [formData, setFormData] = useState({
+        fname: "",
+        lname: "",
+        email: "",
+        street: "",
+        city: "",
+        state: "",
+        zipCode: "",
+        country: "",
+        phone: ""
+    });
+    const handleSubmit=async()=>{
+
+    }
     return (
         <>
            
@@ -8,32 +23,32 @@ function PlaceOrder() {
                 <div className='flex flex-col gap-4 w-full  sm:max-w-[480px] ml-20 mt-10'>
                     <h1 className='text-3xl font-semibold my-3 mx-20'>Delivery Information</h1>
                     <div className='flex  justify-between gap-2'>
-                        <input type="text" placeholder='first name' name="fname" className='p-4 w-full h-10 border-2 border-gray-300 rounded' />
-                        <input type="text" placeholder='last name' name="lname" className='p-4 w-full h-10 border-2  border-gray-300 rounded' />
+                        <input type="text" placeholder='first name' name="fname" value={formData.fname} className='p-4 w-full h-10 border-2 border-gray-300 rounded' />
+                        <input type="text" placeholder='last name' name="lname"   value={formData.lname} className='p-4 w-full h-10 border-2  border-gray-300 rounded' />
 
                     </div>
                     <div>
-                        <input type="email" placeholder='Email' name="email" className='p-4 w-full h-10 border-2  border-gray-300 rounded' />
+                        <input type="email" placeholder='Email' name="email"  value={formData.email} className='p-4 w-full h-10 border-2  border-gray-300 rounded' />
                     </div>
                     <div>
-                        <input type="text" placeholder='Street' name="street" className='p-4 w-full h-10   border-2 border-gray-300 rounded' />
+                        <input type="text" placeholder='Street' name="street"   value={formData.street} className='p-4 w-full h-10   border-2 border-gray-300 rounded' />
                     </div>
                     <div className='flex  justify-between gap-2'>
-                        <input type="text" placeholder='City' name="city" className='p-4 w-full h-10 border-2 border-gray-300 rounded' />
+                        <input type="text" placeholder='City' name="city"  value={formData.city}  className='p-4 w-full h-10 border-2 border-gray-300 rounded' />
                         <input type="text" placeholder='State' name="state" className='p-4 w-full h-10 border-2  border-gray-300 rounded' />
 
                     </div>
                     <div className='flex  justify-between gap-2'>
-                        <input type="number" name="zipcode" placeholder='Zipcode' className='p-4 w-full h-10 border-2 border-gray-300 rounded' />
-                        <input type="text" name="country" placeholder='Country' className='p-4 w-full h-10 border-2  border-gray-300 rounded' />
+                        <input type="number" name="zipCode" placeholder='Zipcode' value={formData.zipCode} className='p-4 w-full h-10 border-2 border-gray-300 rounded' />
+                        <input type="text" name="country" placeholder='Country' value={formData.country} className='p-4 w-full h-10 border-2  border-gray-300 rounded' />
 
                     </div>
                     <div>
-                        <input type="Phone" placeholder=' Contact Number' name="Phone" className='p-3 w-full h-10   border-2 border-gray-300 rounded' />
+                        <input type="Phone" placeholder=' Contact Number' value={formData.phone} name="Phone" className='p-3 w-full h-10   border-2 border-gray-300 rounded' />
 
                     </div>
                     <div className='self-center'>
-                        <button className=' w-72 h-10 text-center cursor-pointer  bg-black text-white rounded-md'>
+                        <button onClick={handleSubmit} className=' w-72 h-10 text-center cursor-pointer  bg-black text-white rounded-md'>
                             Save Address
                         </button>
                     </div>
